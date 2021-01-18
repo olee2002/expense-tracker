ebc28d39ac1fa2e98d6afb6bcbec6b7e26c7ee5d
babylee2002 <babylee2002@gmail.com>
2021-01-17 21:28:03 -0500

    adding github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
-import { React } from 'react';
-
+import { React } from 'react'
 
 function Header() {
    return (
-      <div>
+      <div className='header-container'>
          Expense Report
+         <a
+            className='footer'
+            href='https://my-expense-report.netlify.app/'
+            target='_blank'
+            title='GitHub'>
+            [
+            <img className='img' src='images/github.png' alt='Github' />
+            My Github page]
+         </a>
       </div>
-   );
+   )
 }
 
-export default Header;
+export default Header
